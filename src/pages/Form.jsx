0514976365272fb4f40c1053f5d2cd87ce838025
Gospel Essien <gospelessien627@gmail.com>
2026-08-01 22:43:21 +0100

    added admission

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -2,23 +2,41 @@ import React, { useState } from 'react'
 import {  Link } from 'react-router-dom'
 import "./Form.css"
 import { auth } from '../firebase'
-import { collection, doc, setDoc } from 'firebase/firestore'
+import { doc, setDoc } from 'firebase/firestore'
 import { db } from '../firebase'
 
 
 const Form = () => {
   
 const [status, setStatus] = useState("");
-
+const [firstName, setFirstName] = useState("");
+const [lastName, setLastName] = useState("");
+const [email, setEmail] = useState("");
+const [middleName, setMiddleName] = useState("")
+const [phone, setPhone] = useState("");
+const [address, setAddress] = useState("");
+const [occupation, setOccupation] = useState("");
+const [paretName, setParentName] = useState("");
+const [residentialAddress, setResidentialAddress] = useState("");
+const [dateob, setDateob] = useState("");
 
 
 async function createStudent() {
   try {
-    await setDoc(doc(db, "students", "student1"),
+    await setDoc(doc(db, "students", auth.currentUser.uid),
     {
-      name : "john Doe",
-      age : 20,
-      course : "computer science"
+      firstName,
+      lastName,
+      email,
+      phone,
+      address,
+      middleName,
+      occupation,
+      paretName, 
+      residentialAddress,
+      dateob
+
+    
     }
   );
 
@@ -53,9 +71,9 @@ setStatus("Student created successfully");
         <div className="head">
             <h2> STUDENT ADMISSION FORM </h2>
             <span>Personal Information</span>
-            <h3>First Name: <input type="text" className='pp'/> </h3>
-            <h3>Last Name: <input type="text"  className='pp'/></h3>
-            <h3>Middle Name: <input type="text"  className='pp'/></h3>
+            <h3>First Name: <input type="text" value={firstName} className='pp'/> </h3>
+            <h3>Last Name: <input type="text" value={lastName} className='pp'/></h3>
+            <h3>Middle Name: <input type="text" value={middleName} className='pp'/></h3>
             <h3></h3>
             <label>
               <input  type="checkbox" name='text' />
@@ -65,15 +83,15 @@ setStatus("Student created successfully");
               <input type="checkbox" name='text' />
               female
             </label>
-            <h3>Date of birth :<input type="text" className='pp' /></h3>
-            <h3>Phone Number :<input type="text"  className='pp'/></h3>
-            <h3>Email address :<input type="text"  className='pp'/></h3>
-            <h3>Residential Address :<input type="text" className='pp'/></h3>
+            <h3>Date of birth :<input type="text" value={dateob} className='pp' /></h3>
+            <h3>Phone Number :<input type="text" value={phone} className='pp'/></h3>
+            <h3>Email address :<input type="text" value={email}  className='pp'/></h3>
+            <h3>Residential Address :<input type="text" value={residentialAddress} className='pp'/></h3>
             <hr />
-            <h1>Parent Name:<input type="text"  className='pp'/></h1>
-              <h3>Phone Number:<input type="text"  className='pp'/></h3>
-              <h3>Occupation:<input type="text"  className='pp'/> </h3>
-              <h3>Address:<input type="text"className='pp' /></h3>
+            <h1>Parent Name:<input type="text" value={paretName}  className='pp'/></h1>
+              <h3>Phone Number:<input type="text" value={phone} className='pp'/></h3>
+              <h3>Occupation:<input type="text" value={occupation} className='pp'/> </h3>
+              <h3>Address:<input type="text"className='pp' value={address} /></h3>
                   <hr />
                   <h2>Academic Information</h2>
                <label>

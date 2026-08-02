e7aa610fb150ad3a30d56d92bfb0890131383dd9
Gospel Essien <gospelessien627@gmail.com>
2026-08-02 07:30:18 +0100

    changed the input value

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -71,9 +71,9 @@ setStatus("Student created successfully");
         <div className="head">
             <h2> STUDENT ADMISSION FORM </h2>
             <span>Personal Information</span>
-            <h3>First Name: <input type="text" value={firstName} className='pp'/> </h3>
-            <h3>Last Name: <input type="text" value={lastName} className='pp'/></h3>
-            <h3>Middle Name: <input type="text" value={middleName} className='pp'/></h3>
+            <h3>First Name: <input type="text" onChange={(e) => setFirstName(e.target.value)} value={firstName} className='pp'/> </h3>
+            <h3>Last Name: <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} className='pp'/></h3>
+            <h3>Middle Name: <input type="text" value={middleName} onChange={(e) => setMiddleName(e.target.value)} className='pp'/></h3>
             <h3></h3>
             <label>
               <input  type="checkbox" name='text' />
@@ -83,15 +83,15 @@ setStatus("Student created successfully");
               <input type="checkbox" name='text' />
               female
             </label>
-            <h3>Date of birth :<input type="text" value={dateob} className='pp' /></h3>
-            <h3>Phone Number :<input type="text" value={phone} className='pp'/></h3>
-            <h3>Email address :<input type="text" value={email}  className='pp'/></h3>
-            <h3>Residential Address :<input type="text" value={residentialAddress} className='pp'/></h3>
+            <h3>Date of birth :<input type="text" onChange={(e) => setDateob(e.target.value)} value={dateob} className='pp' /></h3>
+            <h3>Phone Number :<input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} className='pp'/></h3>
+            <h3>Email address :<input type="text" onChange={(e) => setAddress(e.target.value)} value={email}  className='pp'/></h3>
+            <h3>Residential Address :<input type="text" value={residentialAddress} onChange={(e) => setResidentialAddress(e.target.value)} className='pp'/></h3>
             <hr />
-            <h1>Parent Name:<input type="text" value={paretName}  className='pp'/></h1>
-              <h3>Phone Number:<input type="text" value={phone} className='pp'/></h3>
+            <h1>Parent Name:<input type="text" value={paretName} onChange={(e) => setParentName(e.target.value)} className='pp'/></h1>
+              <h3>Phone Number:<input type="text" value={phone} onChange={(e) => setOccupation(e.target.value)} className='pp'/></h3>
               <h3>Occupation:<input type="text" value={occupation} className='pp'/> </h3>
-              <h3>Address:<input type="text"className='pp' value={address} /></h3>
+              <h3>Address:<input type="text"className='pp' onChange={(e) => setAddress(e.target.value)} value={address} /></h3>
                   <hr />
                   <h2>Academic Information</h2>
                <label>
